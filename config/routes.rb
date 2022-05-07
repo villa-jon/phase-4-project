cdb1a3d6a2737b6938521c48554e9fcd2f0b6e32
crystal <noelani.villanueva@gmail.com>
2022-05-06 21:48:57 -0700

    added serializers

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do 
     resources :blogs, only: [:index, :show, :update, :destroy] 
-    # resources :users, only: [:index, :show, :update]
+    resources :users, only: [:index, :show, :update]
     post "/login", to: "sessions#create"
     delete "/logout", to: "sessions#destroy"
   end 
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
-  resources :blogs, only: [:index, :show, :update, :destroy] 
-  resources :users, only: [:index, :show, :update]
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
