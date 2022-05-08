ee4864a1682feaedf0facd7e32c24df72d496e1b
crystal <noelani.villanueva@gmail.com>
2022-05-08 13:22:34 -0700

    it's Jon, currently debugging

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 // import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import Navbar from './Navbar'
 import Signin from './Signin'
 
 function App() {
+  const [newUser, setNewUser] = useState(null)
+
+  
+
+
   return (
     <div className="App">
       <Navbar/>
